f7bfa60f7546ebf192ef72d1ce1a98b41abec4cc
Emiliano de Aguiar <53939021+eaguiar15@users.noreply.github.com>
2024-01-19 13:16:24 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,10 +6,14 @@ function menu(pOpt){
           show("tab-create-game");
           break;
         case "Entrar Partida":
-            break;
+          hide("tab-menu");
+          show("tab-join-game");
+          document.forms[1].P_ID_GAME.focus();
+          break;
         default:
             show("tab-menu");
             hide("tab-create-game");
+            hide("tab-join-game");
           break;
       }
       console.log("essa IDE é foda");
