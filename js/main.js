1dc75caa7cc15fce471455d43ca44ada0f50aa13
Emiliano de Aguiar <53939021+eaguiar15@users.noreply.github.com>
2024-01-26 11:35:14 -0300

    Delete js/main.js

--- js/main.js
+++
@@ -1,5 +0,0 @@
-function showPotCard(pCard){
-    getElem("verse-card").style.backgroundImage = "url('./img/cards/" + pCard + ".png')";
-    const card = document.getElementById('card-pot');
-    card.classList.toggle('flipper');
-}
